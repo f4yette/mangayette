80aa367b9acb5fdd6c52ce44a5daa65cdcfce4c5
ahm3ddd-1 <ahmedahmedd1012@outlook.com>
2025-10-14 00:25:19 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -80,7 +80,7 @@ function Home() {
       <form onSubmit={handleSearch} className="search-form">
         <input
           type="text"
-          placeholder="Search for Mangas..."
+          placeholder="search for mangas..."
           className="search-input"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
