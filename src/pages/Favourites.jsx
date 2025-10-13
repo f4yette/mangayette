a255c8635b3bd992e8a6560d5d362ed8765c83b7
ahm3ddd-1 <ahmedahmedd1012@outlook.com>
2025-10-13 16:58:12 +0100

    Update Favourites.jsx

--- src/pages/Favourites.jsx
+++ src/pages/Favourites.jsx
@@ -3,8 +3,8 @@ import "../css/Favourites.css";
 function Favourites() {
   return (
     <div className="favourties-empty">
-      <h2>No Favourite Mangas yet</h2>
-      <p>Add Mangas to your favourites and they will appear here</p>
+      <h2>no favourite mangas yet</h2>
+      <p>add mangas to your favourites and they will appear here</p>
     </div>
   );
 }
